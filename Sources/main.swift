f8ff459fadaca72926e798ee6884713f8c6c3013
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2016-02-06 17:20:42 +0100

    Add constant flags

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,3 +1,13 @@
+struct Constants {
+  static let NEGATIVE_FLAG: UInt8 = 0b10000000
+  static let OVERFLOW_FLAG: UInt8 = 0b01000000
+  static let BREAK_FLAG: UInt8    = 0b00010000
+  static let DECIMAL_FLAG: UInt8  = 0b00001000
+  static let IRQ_FLAG: UInt8      = 0b00000100
+  static let ZERO_FLAG: UInt8     = 0b00000010
+  static let CARRY_FLAG: UInt8    = 0b00000001
+}
+
 struct CPU {
   var pc: UInt16 = 0
   var sp: UInt8 = 0
