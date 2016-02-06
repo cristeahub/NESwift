de2c599a94512f480b679985aae2428bd51f13f8
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2016-02-06 16:39:03 +0100

    Create initial CPU design

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,11 @@
-print("Hello, world!")
+struct CPU {
+  var pc: UInt16 = 0
+  var sp: UInt8 = 0
+  var a: UInt8 = 0
+  var x: UInt8 = 0
+  var y: UInt8 = 0
+  var p: UInt8 = 0 // status register, layout as follows:
+                   // N V _ B D I Z C
+
+  var memory = [UInt16].init(count: 64_000, repeatedValue: 0)
+}
